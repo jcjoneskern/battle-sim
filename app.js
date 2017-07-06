cc9fcb5b4735b75e7a5c82cf3379e342fe94130c
J-C <jcjoneskern@gmail.com>
2017-07-05 22:21:59 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+(function() {
+
+  angular.module('pkmn', [])
+
+  .controller('battleCtrl', function($scope) {
+    
+  });
+
+})();
+
+/*
+todo:
+trainer pkmn objects
+enemy pkmn objects
+
+functions:
+fight
+bag
+pokemon
+run
+
+visuals:
+pkmn images/animations
+health bars
+
+gettin fancy:
+status
+
+*/
