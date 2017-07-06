1d6cc570603009d6523cd87e444a89f707d51230
J-C <jcjoneskern@gmail.com>
2017-07-05 23:44:35 -0400

    improved run function

--- app.js
+++ app.js
@@ -3,44 +3,80 @@
   angular.module('pkmn', [])
 
   .controller('battleCtrl', function($scope) {
-    let party = [
+    let battleState = true;
+
+    const moves = [
+      {
+        name: 'Thundershock',
+        mult: 1.2
+      },
+      {
+        name: 'Tackle',
+        mult: 1
+      },
+      {
+        name: 'Razor Leaf',
+        mult: 1.2
+      },
+      {
+        name: 'Ember',
+        mult: 1.2
+      },
+      {
+        name: 'Bubble',
+        mult: 1.2
+      },
+      {
+        name: 'Peck',
+        mult: 1.1
+      },
+    ]
+
+    const party = [
       {
         name: 'Pikachu',
         hp: 20,
-        maxhp: 20
+        maxhp: 20,
+        moves: [moves[0], moves[1]]
       },
       {
         name: 'Bulbasaur',
         hp: 25,
-        maxhp: 25
+        maxhp: 25,
+        moves: [moves[0], moves[2]]
       },
       {
         name: 'Squirtle',
         hp: 25,
-        maxhp: 25
+        maxhp: 25,
+        moves: [moves[0], moves[4]]
       },
       {
         name: 'Charmander',
         hp: 20,
-        maxhp: 20
+        maxhp: 20,
+        moves: [moves[0], moves[3]]
       }
     ];
 
-    let enemies = [
+    const enemies = [
       {
         name: 'Rattata',
         hp: 15,
-        maxhp: 15
+        maxhp: 15,
+        moves: [moves[0]]
       },
       {
         name: 'Pidgey',
         hp: 15,
-        maxhp: 15
+        maxhp: 15,
+        moves: [moves[0], moves[5]]
       },
       {
         name: 'Caterpie',
         hp: 10,
-        maxhp: 10
+        maxhp: 10,
+        moves: [moves[0]]
       }
     ]
 
@@ -51,7 +87,7 @@
     $scope.result = '...';
 
     $scope.fight = function() {
-      if($scope.foe.hp > 0 && $scope.pkmn.hp > 0) {
+      if($scope.foe.hp > 0 && $scope.pkmn.hp > 0 && battleState == true) {
         doDmg();
         if ($scope.foe.hp > 0) {
           takeDmg();
@@ -60,12 +96,19 @@
     }
 
     $scope.run = function() {
-      if (rng() >= 0.75) {
-        $scope.result = 'Got away safely!'
-      } else {
-        $scope.result = 'Couldn\'t get away!'
-        takeDmg();
+      if (battleState == true) {
+        if (rng() >= 0.75) {
+          $scope.result = 'Got away safely!'
+          battleState = false;
+        } else {
+          $scope.result = 'Couldn\'t get away!'
+          takeDmg();
+        }
       }
+    }
+
+    function atk() {
+      return Math.floor(Math.random() * 5) + 1;
     }
 
     function doDmg() {
@@ -73,6 +116,7 @@
       if ($scope.foe.hp <= 0) {
         $scope.foe.hp = 0;
         $scope.result = 'You win!';
+        battleState = false;
       }
     }
 
@@ -81,12 +125,8 @@
       if ($scope.pkmn.hp <= 0) {
         $scope.pkmn.hp = 0;
         $scope.result = 'TRAINER whited out!';
+        battleState = false;
       }
-    }
-
-
-    function atk() {
-      return Math.floor(Math.random() * 5) + 1;
     }
 
     function rng() {
@@ -102,7 +142,7 @@ trainer pkmn objects
 enemy pkmn objects
 moves
 items
-experience
+experience/levels
 
 functions:
 fight
@@ -116,8 +156,9 @@ health bars
 
 gettin fancy:
 status
-effectiveness
+effectiveness/types
 accuracy
 speed?
+catching pokemon??
 
 */
