fddf1b8f8598d96700a1c4514ac056a63da4fe06
J-C <jcjoneskern@gmail.com>
2017-07-05 23:06:42 -0400

    basic fight functionality works

--- app.js
+++ app.js
@@ -3,7 +3,40 @@
   angular.module('pkmn', [])
 
   .controller('battleCtrl', function($scope) {
-    
+    $scope.pkmn = {
+      name: 'Pikachu',
+      hp: 20,
+      maxhp: 20
+    };
+
+    $scope.foe = {
+      name: 'Rattata',
+      hp: 15,
+      maxhp: 15
+    };
+
+    $scope.result = '...';
+
+    $scope.fight = function() {
+      if($scope.foe.hp > 0 && $scope.pkmn.hp > 0) {
+        $scope.foe.hp -= atk();
+        if ($scope.foe.hp <= 0) {
+          $scope.foe.hp = 0;
+          $scope.result = 'You win!';
+        } else {
+          $scope.pkmn.hp -= atk();
+          if ($scope.pkmn.hp <= 0) {
+            $scope.pkmn.hp = 0;
+            $scope.result = 'TRAINER whited out!';
+          }
+        }
+      }
+    }
+
+
+    function atk() {
+      return Math.floor(Math.random() * 5) + 1;
+    }
   });
 
 })();
@@ -12,6 +45,9 @@
 todo:
 trainer pkmn objects
 enemy pkmn objects
+moves
+items
+experience
 
 functions:
 fight
@@ -25,5 +61,8 @@ health bars
 
 gettin fancy:
 status
+effectiveness
+accuracy
+speed?
 
 */
