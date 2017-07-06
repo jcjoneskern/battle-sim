505a556489d04d0ac3d2e47d52a75f430c2fd700
J-C <jcjoneskern@gmail.com>
2017-07-05 23:26:33 -0400

    run function, lots of refactoring

--- app.js
+++ app.js
@@ -3,39 +3,94 @@
   angular.module('pkmn', [])
 
   .controller('battleCtrl', function($scope) {
-    $scope.pkmn = {
-      name: 'Pikachu',
-      hp: 20,
-      maxhp: 20
-    };
+    let party = [
+      {
+        name: 'Pikachu',
+        hp: 20,
+        maxhp: 20
+      },
+      {
+        name: 'Bulbasaur',
+        hp: 25,
+        maxhp: 25
+      },
+      {
+        name: 'Squirtle',
+        hp: 25,
+        maxhp: 25
+      },
+      {
+        name: 'Charmander',
+        hp: 20,
+        maxhp: 20
+      }
+    ];
 
-    $scope.foe = {
-      name: 'Rattata',
-      hp: 15,
-      maxhp: 15
-    };
+    let enemies = [
+      {
+        name: 'Rattata',
+        hp: 15,
+        maxhp: 15
+      },
+      {
+        name: 'Pidgey',
+        hp: 15,
+        maxhp: 15
+      },
+      {
+        name: 'Caterpie',
+        hp: 10,
+        maxhp: 10
+      }
+    ]
+
+    $scope.pkmn = party[0];
+
+    $scope.foe = enemies[0];
 
     $scope.result = '...';
 
     $scope.fight = function() {
       if($scope.foe.hp > 0 && $scope.pkmn.hp > 0) {
-        $scope.foe.hp -= atk();
-        if ($scope.foe.hp <= 0) {
-          $scope.foe.hp = 0;
-          $scope.result = 'You win!';
-        } else {
-          $scope.pkmn.hp -= atk();
-          if ($scope.pkmn.hp <= 0) {
-            $scope.pkmn.hp = 0;
-            $scope.result = 'TRAINER whited out!';
-          }
+        doDmg();
+        if ($scope.foe.hp > 0) {
+          takeDmg();
         }
+      }
+    }
+
+    $scope.run = function() {
+      if (rng() >= 0.75) {
+        $scope.result = 'Got away safely!'
+      } else {
+        $scope.result = 'Couldn\'t get away!'
+        takeDmg();
+      }
+    }
+
+    function doDmg() {
+      $scope.foe.hp -= atk();
+      if ($scope.foe.hp <= 0) {
+        $scope.foe.hp = 0;
+        $scope.result = 'You win!';
+      }
+    }
+
+    function takeDmg() {
+      $scope.pkmn.hp -= atk();
+      if ($scope.pkmn.hp <= 0) {
+        $scope.pkmn.hp = 0;
+        $scope.result = 'TRAINER whited out!';
       }
     }
 
 
     function atk() {
       return Math.floor(Math.random() * 5) + 1;
+    }
+
+    function rng() {
+      return Math.random().toFixed(2);
     }
   });
 
